4155a3a77508ebb7fe6dd4cda3404d27dc8753dd
Geno446 <daniel.orford@activerisk.com>
2013-11-09 00:14:09 +0000

    Some basic class

---
+++ LashLad/LashLad/Entity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LashLad
+{
+    class Entity
+    {
+
+        public int XPosition;
+        private int x
+        {
+            get;
+            set;
+        }
+
+        public int YPosition;
+        private int y
+        {
+            get;
+            set;
+        }
+
+    }
+}
