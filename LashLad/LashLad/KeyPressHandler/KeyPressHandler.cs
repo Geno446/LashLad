f9ca196a5ef705021c717f92974cae0f1b0cb205
Medic1de <sinpie29@hotmail.com>
2013-11-09 01:23:59 +0000

    Added KeyboardHandler that handles WASD keystrokes from the form

---
+++ LashLad/LashLad/KeyPressHandler/KeyPressHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LashLad
+{
+    class KeyPressHandler
+    {
+        private KeyPressHandler(){ }
+
+        public KeyPressHandler(Keys keys)
+        {
+            switch (keys)
+            {
+                case Keys.W:
+                    //call player.move
+                    break;
+                case Keys.A:
+                    break;
+                case Keys.S:
+                    break;
+                case Keys.D:
+                    break;
+            }
+        }
+    }
+}
