317691458a76c5414b6201c2509ea66452e2549d
Gaz <garethmu@gmail.com>
2013-11-09 01:04:38 +0000

    Added GetSprite method which takes IEntity and returns a bitmap representing it.

---
+++ LashLad/LashLad/View/Sprites/ISprite.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+
+namespace LashLad.View.Sprites
+{
+    interface ISprite
+    {
+        Bitmap GetSprite(Model.IEntity Entity);
+    }
+}
